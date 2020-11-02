239071c0d96418a44659af32508a1d9d81e2c267
Alena Trushnikova <alena.yu.t@gmail.com>
2020-11-02 23:12:14 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,10 +1,46 @@
+require 'pry'
 require_relative './part_1_solution.rb'
+
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  
+  cart = 	[
+		{"AVOCADO" => {:price => 3.00, :clearance => true}},
+		{"KALE" => {:price => 3.00, :clearance => false}},
+		{"AVOCADO" => {:price => 3.00, :clearance => true}},
+		{"AVOCADO" => {:price => 3.00, :clearance => true}},
+		{"BEETS" => {:price => 2.50, :clearance => false}}
+	]
+	
+	coupons = [
+		{:item => "AVOCADO", :num => 2, :cost => 5.00},
+		{:item => "BEER", :num => 2, :cost => 20.00},
+		{:item => "CHEESE", :num => 3, :cost => 15.00}
+	]
+	
+index = 0
+new_cart = consolidate_cart(cart)
+
+
+  while index < coupons.count do
+      coupon = coupons[index]
+      item_with_coupon = find_item_by_name_in_collection(coupon[:item], new_cart)
+      if item_with_coupon != nil && item_with_coupon[:count] >= coupon[:num]
+        coupon_count = (item_with_coupon[:count] / coupon[:num]).round_down
+        item_with_coupon[:count] = coupon_count
+        
+        new_cart << item_with_coupon
+      end  
+    index += 1
+  end
+  new_cart 
+ 
 end
+
+
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
